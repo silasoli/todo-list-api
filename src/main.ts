1083cb0a308bc1e3a7ad48c90e3ec80ce3a7d4b7
silasoli <silasoliv39@gmail.com>
2024-06-03 14:18:19 -0300

    fix: error in deploy docs

--- src/main.ts
+++ src/main.ts
@@ -22,13 +22,22 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
 
+ 
   SwaggerModule.setup('docs', app, document, {
     swaggerOptions: {
+      persistAuthorization: true,
       tagsSorter: 'alpha',
       operationsSorter: '',
     },
+    customfavIcon:
+      'https://cdn.jsdelivr.net/npm/swagger-ui-dist@3.25.0/favicon-32x32.png',
+    customCssUrl:
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.0/swagger-ui.min.css',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.0/swagger-ui-bundle.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.0/swagger-ui-standalone-preset.js',
+    ],
   });
-
 
   const port = configService.get('PORT');
   if (!port) throw new Error("Application port wasn't found");
